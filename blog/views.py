6f549dbf75c6d55d0f9abe5f1deb39a0e83a4fe2
Myndset88 <m1ndset@pm.me>
2023-01-09 21:39:12 -0500

    implement-search

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, \
-    PageNotAnInteger
+                                  PageNotAnInteger
 from django.views.generic import ListView
-from blog.forms import EmailPostForm, CommentForm
+from django.contrib.postgres.search import SearchVector, \
+                                           SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -118,3 +121,26 @@ def post_comment(request, post_id):
                            {'post': post,
                             'form': form,
                             'comment': comment})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
